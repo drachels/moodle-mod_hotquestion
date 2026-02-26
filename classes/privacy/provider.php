e09e28891f0f9c2767c51062952ed5d018316f55
Luca Bösch <luca.boesch@bfh.ch>
2026-02-26 15:40:53 +0100

    Order classes/privacy/provider.php implementations properly.

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -50,8 +50,8 @@ defined('MOODLE_INTERNAL') || die(); // @codingStandardsIgnoreLine
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements
-    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\request\plugin\provider {
     /**
      * Get a description of the data stored by this plugin.
